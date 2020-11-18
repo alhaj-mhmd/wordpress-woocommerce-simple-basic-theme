f28e3b20582c131ee2c80d90baed7ee4f47dca6e
Mohammad <alhaj.mhmd@gmail.com>
2020-11-19 00:29:01 +0800

    more with wordpress functions
    language_attributes()
    echo bloginfo( "charset" )
    bloginfo( "title" )

--- wp-content/themes/simplebasic/header.php
+++ wp-content/themes/simplebasic/header.php
@@ -1,10 +1,10 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes() ?>>
 
 <head>
-    <meta charset="UTF-8">
+    <meta charset="<?php echo bloginfo( "charset" ) ?>">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Simple Basic</title>
+    <title><?php echo bloginfo( "title" ) ?></title>
 </head>
 
 <body>
